648a8bc4d2e1b2f1b8739f5632fa0171ea2d367b
wangtong <wtgeeker@163.com>
2020-01-19 13:20:22 +0800

    travis

--- bin/test.sh
+++ bin/test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set -x
+wget -O chromedriver_linux64.zip -q https://npm.taobao.org/mirrors/chromedriver/79.0.3945.36/chromedriver_linux64.zip
 
-wget -q https://npm.taobao.org/mirrors/chromedriver/80.0.3987.16/chromedriver_linux64.zip
+unzip -uxo chromedriver_linux64.zip -d /opt
 
-unzip -x chromedriver_linux64.zip -d /opt
-
-sbt ';clean ;coverage ;test ;coverageReport'
+chmod +x /opt/chromedriver
 
 rm chromedriver_linux64.zip
+
+sbt ';clean ;coverage ;test ;coverageReport'
